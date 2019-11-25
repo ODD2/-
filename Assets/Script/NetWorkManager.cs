ada0b9c4273145cd17e31bc2afecbcfe24f73be4
Teddy12155555 <mickey23405383@gmail.com>
2019-11-25 12:32:03 +0800

    GUI

---
+++ Assets/Script/NetWorkManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Photon.Pun;
+public class NetWorkManager : MonoBehaviourPunCallbacks
+{
+    public GameObject connect;
+    public GameObject disconnect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    public void onClick()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        connect.SetActive(true);
+    }
+    
+}
