dcedb14bfdcd9d5e4d5afeee958751a715f3fda5
eric88525 <eric885251@gmail.com>
2019-12-21 15:19:12 +0800

    粒子自銷毀 特效持續時間調整

---
+++ Assets/ZD/Script/Effect/DestroyParticles.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyParticles : MonoBehaviour
+{
+
+    private void Start()
+    {
+        
+        Destroy(gameObject, GetComponent<ParticleSystem>().main.duration);
+    }
+
+}
