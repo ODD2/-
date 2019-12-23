d573e23deb08eed5c4df79188681008267528c18
Teddy12155555 <mickey23405383@gmail.com>
2019-12-23 20:30:02 +0800

    Teaching view and Digang animation

---
+++ Assets/ZD/Script/GameFlow/TeachingView.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TeachingView : MonoBehaviour
+{
+    public Sprite[] TeachImg;
+    public Image View;
+    public GameObject Left;
+    public GameObject Right;
+
+    private int index = 0;
+
+    void Start()
+    {
+        index = 0;
+        Left.GetComponent<Button>().onClick.AddListener(() => ToLeft());
+        Right.GetComponent<Button>().onClick.AddListener(() => ToRight());
+        View.sprite = TeachImg[index];
+        UpdateBut();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void ToLeft()
+    {
+        if (index > 0)
+            index -= 1;
+        UpdateBut();
+        View.sprite = TeachImg[index];
+    }
+    void ToRight()
+    {
+        if (index < 6)
+            index += 1;
+        UpdateBut();
+        View.sprite = TeachImg[index];
+    }
+    void UpdateBut()
+    {
+        if (index == 0) Left.SetActive(false);
+        else Left.SetActive(true);
+        if (index == TeachImg.Length-1) Right.SetActive(false);
+        else Right.SetActive(true);
+    }
+}
